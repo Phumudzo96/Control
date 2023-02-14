83f5c19831658fb0e035a36fd78ac1b66be22dd3
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-14 17:45:18 +0200

    Add files via upload

---
+++ control.py
@@ -0,0 +1,10 @@
+#ak the user to enter their age
+age = int(input("Enter your age: "))
+
+#Use the if-elif-else statement to find out where the user belongs
+if age >= 18:
+    print("You are old enough")
+elif age >= 16 != 18:
+    print("Almost there")
+else:
+    print("You're just too young")
